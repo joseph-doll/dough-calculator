d9be58977ea2bc92e2d9cf01f0ff70dc8b3d2c66
Joseph Doll <jdoll003@gmail.com>
2021-02-27 12:29:37 -0800

    Revision 0.9

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -17,7 +17,6 @@ function toMake() {
     document.getElementById('Make').innerHTML = Math.round(need * 10) / 10;
 }
 
-
 function need() {
     var LSN = document.getElementById("LSN").value;
     var MSN = document.getElementById("MSN").value;
@@ -27,18 +26,16 @@ function need() {
     document.getElementById('Tomorrow Buckets').innerHTML = Math.round(buckets * 10) /10;
 }
 
-
 function tomorrow() {
     var d = new Date();
     var weekday = new Array(7);
-    weekday[6] = "SUNDAY";
+    weekday[6] = "SUNDAYS";
     weekday[0] = "MONDAY";
     weekday[1] = "TUESDAY";
     weekday[2] = "WEDNESDAY";
     weekday[3] = "THURSDAY";
     weekday[4] = "FRIDAY";
     weekday[5] = "SATURDAY";
-  
     var n = weekday[d.getDay()];
     document.getElementById("Tomorrow").innerHTML = n;
 }
